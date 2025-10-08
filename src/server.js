5cb0962e4fe0e2a11a4fbdb2551a6498d5a975ff
Henrique Francisco <Henriquesouza432@outlook.com>
2025-10-08 07:46:10 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import app from './app.js';
 import './database/index.js';
 
-app.listen(3001, () => console.log('Application is runnig at port 3001'));
+app.listen(process.env.PORT || 3001, () => console.log('Application is runnig at port 3001'));
+
